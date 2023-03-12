79dd88fc74dd81f01191a9fda625402a55a7ef2d
LizOchkaaaa <svrvliza14@gmail.com>
2023-03-12 17:28:52 +0300

    Initial commit

---
+++ src/main/java/org/example/XmlUtils/DateTimeAdapter.java
@@ -0,0 +1,2 @@
+package org.example.XmlUtils;public class DateTimeAdapter {
+}
---
+++ src/main/java/org/example/interfaces/CollectionCustom.java
@@ -0,0 +1,17 @@
+package org.example.Interface;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CollectionCustom<T> {
+    /** validates the list of items*/
+    boolean validateData();
+    /** returns the Stack of T */
+    List<T> get();
+    /** returns the initialization time of the collection */
+    LocalDateTime getInitializationTime();
+    /** returns the type of element in the collection */
+    Class getElementType();
+    /** saves the collection to file */
+    void save();
+}
